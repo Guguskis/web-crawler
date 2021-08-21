3cdd9d43ac471327bb709f268f33bb9f40ed0853
Matas Liutikas <matasliutikas@gmail.com>
2021-08-21 13:16:09 +0300

    init commit

---
+++ src/test/java/lt/liutikas/web/crawler/WebCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.liutikas.web.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
